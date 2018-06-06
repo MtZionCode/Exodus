3320d609404ee1066b17d6a87581412f6411c370
Gebre Mesquitta <23564291+MtZionCode@users.noreply.github.com>
2018-06-05 21:50:43 -0500

    Update Simon Game.js
    Added Simon Game description as well as my goals  for the javascript interactivity of the UI interface

--- Simon Game.js
+++ Simon Game.js
@@ -1,6 +1,22 @@
 
  //under construction......stay tuned
 
+/* My goal is that none of the buttons on the simon game (including the colored pads) should work when Simon Game is off (on/off toggle button)
+
+"Simon Game is a simple game which goal is for user to repeat the pattern showed by the program. 
+With each round a new step is added to the pattern making, the game much more difficult with every round. 
+Additionally, the program should play a different sound for each field that is activated. 
+On top Of that you have to enable user to switch between strict mode, which restarts a game whenever user selects wrong field in a pattern, and normal mode which only repeats the pattern subsequent of the wrong button being pressed. 
+The provided example constituted of 4 different fields with different color for each field and I followed with the same idea."
+*/
+/*
+I might like to achieve my goals using a state method employing usage of MOBX (mobx.js.org/index.htmL) 
+*/
+
+
+
+
+
 function play(sound){
    var audio = document.getElementById("audio");
     audio.setAttribute('src', sound);
